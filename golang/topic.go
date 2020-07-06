76cf4f9f391906e41c24d8c73dea9c11cd1672e6
지농 <jinhong0719@naver.com>
2020-07-06 21:39:02 +0900

    [CREATE] <golang/topic.go> EventTopic 상수 모음 golang 파일 추가

---
+++ golang/topic.go
@@ -0,0 +1,7 @@
+package golang
+
+const (
+	CreateUserEventTopic = "examples.blog.service.user.CreateUser"
+	CreateAuthEventTopic = "examples.blog.service.auth.CreateAuth"
+	ChangeAuthStatusEventTopic = "examples.blog.service.auth.ChangeAuthStatus"
+)
